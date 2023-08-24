65237099a54ce7c083afd2a74885058ef2d8acf0
Valentina061103 <valentinalara.vl06@gmail.com>
2023-08-24 10:55:39 -0700

    ejercicios complementarios

---
+++ ejercicioclase4.py
@@ -0,0 +1,23 @@
+fecha = input("dia, DD/MM: ")
+dia_sem = fecha[0:fecha.find(",")]
+dia = fecha[fecha.find(" ")+1:fecha.find("/")]
+mes = fecha[fecha.find("/")+1:]
+dia_sem = dia_sem.lower()
+
+if dia_sem != "lunes" and dia_sem != "martes" and dia_sem != "miercoles" and dia_sem != "jueves" and dia_sem != "viernes":
+    print("error, dia no valido")
+else:
+    if dia > "31":
+        print("error, numero de dia no valido")
+    else:
+        if mes > "13":
+            print("error,numero de mes no valido")
+        else:
+            print(fecha)
+            if dia_sem : "lunes"
+            nivel_inicial = str(input("¿Hubo examen? "))
+            if nivel_inicial : "si"
+            aprob_n_i = int(input("ingrese cantidad de aprobados: "))
+            desaprob_n_i = int(input("ingrese cantidad de desaprobados: "))
+            aprobados = (aprob_n_i/(aprob_n_i + desaprob_n_i))*100
+            print("El porcentaje de alumnos aprobados es: %",aprobados)
---
+++ ejercicioscompl.py
@@ -0,0 +1,56 @@
+#1
+numero1 = 8
+#2
+numero2 = 10.2
+#3
+suma = numero1 + numero2
+#4
+resta = numero2 - numero1
+multiplicacion = numero1 * numero2
+division = numero2/numero1
+print(numero1, numero2)
+print("la resta es: ",resta)
+print("la multiplicacion es: ", multiplicacion)
+print("la division es: ", division)
+#5
+nombre = "valentina"
+#6
+precio = 100
+#7
+descuento = 0.10
+#8
+cantidad_descuento = precio * descuento
+precio_final = precio - cantidad_descuento
+print(precio_final)
+#9
+cadena = "blood of my blood bone of my bone"
+#10
+longitud = len(cadena)
+print(longitud)
+#11
+precio = 3.5
+precio_en_entero = int(precio)
+print(precio_en_entero)
+#12
+nombre = "Jamie "
+apellido = "Fraser"
+nombre_completo = nombre + apellido
+print(nombre_completo)
+#13
+edad = 19
+edad_5 = edad + 5
+edad_10 = edad_5 - 10
+print(edad_10)
+#14
+altura = 1.53
+altura_4 = altura*4
+altura_3 = altura_4/3
+print(altura_3)
+#15
+nombre_mayus = "VALENTINA"
+nombre_minus = nombre_mayus.lower()
+print(nombre_minus)
+#16
+nombre_minus = nombre_mayus.lower()
+nombre_primer_letra = nombre_minus.capitalize()
+print(nombre_primer_letra)
